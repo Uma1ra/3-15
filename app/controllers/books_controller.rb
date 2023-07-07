98c55a5f76f7bcc9fb278d71d658b4dff7406f52
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-07-07 04:11:56 +0000

    [add] 新規登録機能/create/rails cで保存確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
   
-  def new
-    @new_book = Book.new
-  end
-  
   def create
+    create_book = Book.new(book_params)
+    create_book.save
+    redirect_to "/"
   end
   
   def index
     @index_books = Book.all
+    @new_book = Book.new
   end
 
   def show
